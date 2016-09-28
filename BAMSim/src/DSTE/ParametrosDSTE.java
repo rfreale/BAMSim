f4ee68b9a040d9fd9ed0e73ea2935da199cd3103
David Barreto <davidssb@hotmail.com>
2016-09-28 12:32:25 -0300

    Topologia

--- BAMSim/src/DSTE/ParametrosDSTE.java
+++ BAMSim/src/DSTE/ParametrosDSTE.java
@@ -19,16 +19,16 @@ import jcolibri.method.retrieve.NNretrieval.similarity.local.ontology.OntDetail;
 
 public class ParametrosDSTE {
 	//public static final int MaxInteracoes=50;
-	public static final int MaxCaminhos = 2;
+	public static final int MaxCaminhos = 10;
 	//public static final int MaxOverlapFactor = 5;
-	public static final int MaxSaltos = 2;
+	public static final int MaxSaltos = 10;
 	//public static final int NumEnlacesFullDuplex = 12;
 	public static final int MaxH = 0;
 	public static final int MaxClassType = 3;
-	public static int LINKS = 1; // N�mero de LINKS (Simplex) do Modelo
-	public static int ROTEADORES = 2; // N�mero de roteadores DSTE
+	public static int LINKS = 6; // N�mero de LINKS (Simplex) do Modelo
+	public static int ROTEADORES = 5; // N�mero de roteadores DSTE
 	public static BAMType BAMTypePadrao = BAMType.PreemptionGBAM;  //NoPreemptionMAM  //PreemptionAllocCTSharing  //PreemptionRDM
-	public static final long Janela = 600;
+	public static final long Janela = 30;
 	
 	//SLAs em Percentual
 	public static final long SLAPreempcoes = 5;
@@ -41,8 +41,8 @@ public class ParametrosDSTE {
 	 * DS:ds-name:{GAUGE | COUNTER | DERIVE | DCOUNTER | DDERIVE | ABSOLUTE}:heartbeat:min:max
 	 * RRA:{AVERAGE | MIN | MAX | LAST}:xff:steps:rows
 	 */
-	public static final long RRDStarTime = 1451617200;  //start /// date +"%s"
-	public static final long RRDAmostra = 120;  //step
+	public static final long RRDStarTime = 1451617200-3600;  //start /// date +"%s"
+	public static final long RRDAmostra = 30;  //step
 	public static final long RRDBatida =RRDAmostra*2 ; //heartbeat
 	public static final double RRDMin =Double.NaN;    //valor mínimo fora dos quais não deve ser considerada a leitura.
 	public static final double RRDMax = Double.NaN;   //valor máximo fora dos quais não deve ser considerada a leitura.
@@ -56,21 +56,68 @@ public class ParametrosDSTE {
 	{
 		
 		t.roteador[0]= new Roteador(); 
-		t.roteador[0].ID = 0;
-		t.roteador[0].Descricao = "S1";
+		t.roteador[0].ID = 0; //int
+		t.roteador[0].Descricao = "R0";
 		t.roteador[1]= new Roteador(); 
 		t.roteador[1].ID = 1;
-		t.roteador[1].Descricao = "D1";
-		
-		
-		
+		t.roteador[1].Descricao = "R1";
+		t.roteador[2]= new Roteador(); 
+		t.roteador[2].ID = 2;
+		t.roteador[2].Descricao = "R2";
+		t.roteador[3]= new Roteador(); 
+		t.roteador[3].ID = 3;
+		t.roteador[3].Descricao = "R3";
+		t.roteador[4]= new Roteador(); 
+		t.roteador[4].ID = 4;
+		t.roteador[4].Descricao = "R4";
+				
 		t.link[0]=new Link();
-		t.link[0].Descricao = "S1->D1";
-		t.link[0].ID = 1;
+		t.link[0].Descricao = "R0->R4";
+		t.link[0].ID = 0;
 		t.link[0].CustoEnlace = 1;
-		t.link[0].CargaEnlace = 1000;
+		t.link[0].CargaEnlace = 1000; // 1 GB
 		t.link[0].lsrSrc = t.roteador[0];
-		t.link[0].lsrDest = t.roteador[1];		
+		t.link[0].lsrDest = t.roteador[4];
+		
+		t.link[1]=new Link();
+		t.link[1].Descricao = "R0->R1";
+		t.link[1].ID = 1;
+		t.link[1].CustoEnlace = 1;
+		t.link[1].CargaEnlace = 1000; // 1 GB
+		t.link[1].lsrSrc = t.roteador[0];
+		t.link[1].lsrDest = t.roteador[1];
+		
+		t.link[2]=new Link();
+		t.link[2].Descricao = "R1->R4";
+		t.link[2].ID = 2;
+		t.link[2].CustoEnlace = 1;
+		t.link[2].CargaEnlace = 1000; // 1 GB
+		t.link[2].lsrSrc = t.roteador[1];
+		t.link[2].lsrDest = t.roteador[4];
+		
+		t.link[3]=new Link();
+		t.link[3].Descricao = "R0->R2";
+		t.link[3].ID = 3;
+		t.link[3].CustoEnlace = 1;
+		t.link[3].CargaEnlace = 1000; // 1 GB
+		t.link[3].lsrSrc = t.roteador[0];
+		t.link[3].lsrDest = t.roteador[2];
+		
+		t.link[4]=new Link();
+		t.link[4].Descricao = "R2->R3";
+		t.link[4].ID = 4;
+		t.link[4].CustoEnlace = 1;
+		t.link[4].CargaEnlace = 1000; // 1 GB
+		t.link[4].lsrSrc = t.roteador[2];
+		t.link[4].lsrDest = t.roteador[3];
+		
+		t.link[5]=new Link();
+		t.link[5].Descricao = "R3->R4";
+		t.link[5].ID = 5;
+		t.link[5].CustoEnlace = 1;
+		t.link[5].CargaEnlace = 1000; // 1 GB
+		t.link[5].lsrSrc = t.roteador[3];
+		t.link[5].lsrDest = t.roteador[4];		
 		
 
 	}
@@ -78,7 +125,39 @@ public class ParametrosDSTE {
 	public static void matrizDeCaminhosManual(Topologia t)
 	{
 		
-		t.adicionar(0,0,0);
+		// Matriz de caminhos R0 para R4 
+		// Caminho [0] [0]
+		// R0->R4
+		t.adicionar(0,0,0); // roteadorID,  caminhoID,  linkID
+		// Caminho [0] [1]
+		// R0->R1
+		t.adicionar(0,1,1);
+		// R1->R4
+		t.adicionar(0,1,2);
+		// Caminho [0] [2]
+		// R0->R2
+		t.adicionar(0,2,3);
+		// R2->R3
+		t.adicionar(0,2,4);
+		// R3->R4
+		t.adicionar(0,2,5);
+
+//Matriz de caminhos R1 para R4
+		// Caminho [1] [0]
+		// R1->R4
+		t.adicionar(1,0,2);
+						
+//Matriz de caminhos R2 para R4 	
+		// Caminho [2] [0]
+		// R2->R3
+		t.adicionar(2,0,4);
+		// R3->R4
+		t.adicionar(2,0,5);
+					
+//Matriz de caminhos R3 para R4
+		// Caminho [3] [0]
+		// R3->R4
+		t.adicionar(3,0,5);
 		
 	}
 	
