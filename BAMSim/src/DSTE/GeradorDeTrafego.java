fb62fddc2697e98698634b1652ac74502b30e641
rfreale <reale@ifba.edu.br>
2017-04-19 18:05:47 -0300

    trafegoForcado
    trafegoForcado

--- BAMSim/src/DSTE/GeradorDeTrafego.java
+++ BAMSim/src/DSTE/GeradorDeTrafego.java
@@ -1,12 +1,133 @@
 package DSTE;
 
+import java.io.IOException;
+
+import org.jrobin.core.RrdException;
+
 import Simulador.*;
 
 public class GeradorDeTrafego {
 
 	private static int numeroDeLSPsPorMinuto[][];
 	private static int numeroDeLSPsPorSlot[][];
+	private static int CT0=10;//40
+	private static int CT1=12;//35
+	private static int CT2=8;//25
+	private static int [][]matriz =
+		{ 
+			{CT0+0, CT1+0, CT2+0},	{CT0+1, CT1+0, CT2+0},	{CT0+2, CT1+0, CT2+0},	{CT0+3, CT1+0, CT2+0},	{CT0+4, CT1+0, CT2+0},	{CT0+5, CT1+0, CT2+0},	{CT0+6, CT1+0, CT2+0},	{CT0+7, CT1+0, CT2+0},	{CT0+8, CT1+0, CT2+0},	{CT0+9, CT1+0, CT2+0},	{CT0+10, CT1+0, CT2+0},	{CT0+11, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+14, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+1},	{CT0+11, CT1+0, CT2+2},	{CT0+10, CT1+0, CT2+3},	{CT0+9, CT1+0, CT2+4},	{CT0+8, CT1+0, CT2+5},	{CT0+7, CT1+0, CT2+6},	{CT0+6, CT1+0, CT2+7},	{CT0+5, CT1+0, CT2+8},	{CT0+4, CT1+0, CT2+9},	{CT0+3, CT1+0, CT2+10},	{CT0+2, CT1+0, CT2+11},	{CT0+1, CT1+0, CT2+12},	{CT0+1, CT1+0, CT2+13},	{CT0+0, CT1+0, CT2+14},	{CT0+0, CT1+0, CT2+13},	{CT0+0, CT1+0, CT2+12},	{CT0+0, CT1+0, CT2+11},	{CT0+0, CT1+0, CT2+10},	{CT0+0, CT1+0, CT2+9},	{CT0+0, CT1+0, CT2+8},	{CT0+0, CT1+0, CT2+7},	{CT0+0, CT1+0, CT2+6},	{CT0+0, CT1+0, CT2+5},	{CT0+0, CT1+0, CT2+4},	{CT0+0, CT1+0, CT2+3},	{CT0+0, CT1+0, CT2+2},	{CT0+0, CT1+0, CT2+1},	{CT0+0, CT1+0, CT2+1},	{CT0+0, CT1+0, CT2+0},
+			{CT0+0, CT1+0, CT2+0},	{CT0+1, CT1+0, CT2+0},	{CT0+2, CT1+0, CT2+0},	{CT0+3, CT1+0, CT2+0},	{CT0+4, CT1+0, CT2+0},	{CT0+5, CT1+0, CT2+0},	{CT0+6, CT1+0, CT2+0},	{CT0+7, CT1+0, CT2+0},	{CT0+8, CT1+0, CT2+0},	{CT0+9, CT1+0, CT2+0},	{CT0+10, CT1+0, CT2+0},	{CT0+11, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+14, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+1},	{CT0+11, CT1+0, CT2+2},	{CT0+10, CT1+0, CT2+3},	{CT0+9, CT1+0, CT2+4},	{CT0+8, CT1+0, CT2+5},	{CT0+7, CT1+0, CT2+6},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+10, CT1+0, CT2+12},	{CT0+10, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+10},	{CT0+13, CT1+0, CT2+8},	{CT0+13, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},
+			{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+10, CT1+0, CT2+12},	{CT0+10, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+10},	{CT0+13, CT1+0, CT2+8},	{CT0+13, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},
 
+			{CT0+0, CT1+0, CT2+0},	{CT0+1, CT1+0, CT2+0},	{CT0+2, CT1+0, CT2+0},	{CT0+3, CT1+0, CT2+0},	{CT0+4, CT1+0, CT2+0},	{CT0+5, CT1+0, CT2+0},	{CT0+6, CT1+0, CT2+0},	{CT0+7, CT1+0, CT2+0},	{CT0+8, CT1+0, CT2+0},	{CT0+9, CT1+0, CT2+0},	{CT0+10, CT1+0, CT2+0},	{CT0+11, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+14, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+1},	{CT0+11, CT1+0, CT2+2},	{CT0+10, CT1+0, CT2+3},	{CT0+9, CT1+0, CT2+4},	{CT0+8, CT1+0, CT2+5},	{CT0+7, CT1+0, CT2+6},	{CT0+6, CT1+0, CT2+7},	{CT0+5, CT1+0, CT2+8},	{CT0+4, CT1+0, CT2+9},	{CT0+3, CT1+0, CT2+10},	{CT0+2, CT1+0, CT2+11},	{CT0+1, CT1+0, CT2+12},	{CT0+1, CT1+0, CT2+13},	{CT0+0, CT1+0, CT2+14},	{CT0+0, CT1+0, CT2+13},	{CT0+0, CT1+0, CT2+12},	{CT0+0, CT1+0, CT2+11},	{CT0+0, CT1+0, CT2+10},	{CT0+0, CT1+0, CT2+9},	{CT0+0, CT1+0, CT2+8},	{CT0+0, CT1+0, CT2+7},	{CT0+0, CT1+0, CT2+6},	{CT0+0, CT1+0, CT2+5},	{CT0+0, CT1+0, CT2+4},	{CT0+0, CT1+0, CT2+3},	{CT0+0, CT1+0, CT2+2},	{CT0+0, CT1+0, CT2+1},	{CT0+0, CT1+0, CT2+1},	{CT0+0, CT1+0, CT2+0},
+			{CT0+0, CT1+0, CT2+0},	{CT0+1, CT1+0, CT2+0},	{CT0+2, CT1+0, CT2+0},	{CT0+3, CT1+0, CT2+0},	{CT0+4, CT1+0, CT2+0},	{CT0+5, CT1+0, CT2+0},	{CT0+6, CT1+0, CT2+0},	{CT0+7, CT1+0, CT2+0},	{CT0+8, CT1+0, CT2+0},	{CT0+9, CT1+0, CT2+0},	{CT0+10, CT1+0, CT2+0},	{CT0+11, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+14, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+1},	{CT0+11, CT1+0, CT2+2},	{CT0+10, CT1+0, CT2+3},	{CT0+9, CT1+0, CT2+4},	{CT0+8, CT1+0, CT2+5},	{CT0+7, CT1+0, CT2+6},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+10, CT1+0, CT2+12},	{CT0+10, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+10},	{CT0+13, CT1+0, CT2+8},	{CT0+13, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},
+			{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+10, CT1+0, CT2+12},	{CT0+10, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+10},	{CT0+13, CT1+0, CT2+8},	{CT0+13, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},
+
+
+			{CT0+0, CT1+0, CT2+0},	{CT0+1, CT1+0, CT2+0},	{CT0+2, CT1+0, CT2+0},	{CT0+3, CT1+0, CT2+0},	{CT0+4, CT1+0, CT2+0},	{CT0+5, CT1+0, CT2+0},	{CT0+6, CT1+0, CT2+0},	{CT0+7, CT1+0, CT2+0},	{CT0+8, CT1+0, CT2+0},	{CT0+9, CT1+0, CT2+0},	{CT0+10, CT1+0, CT2+0},	{CT0+11, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+14, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+1},	{CT0+11, CT1+0, CT2+2},	{CT0+10, CT1+0, CT2+3},	{CT0+9, CT1+0, CT2+4},	{CT0+8, CT1+0, CT2+5},	{CT0+7, CT1+0, CT2+6},	{CT0+6, CT1+0, CT2+7},	{CT0+5, CT1+0, CT2+8},	{CT0+4, CT1+0, CT2+9},	{CT0+3, CT1+0, CT2+10},	{CT0+2, CT1+0, CT2+11},	{CT0+1, CT1+0, CT2+12},	{CT0+1, CT1+0, CT2+13},	{CT0+0, CT1+0, CT2+14},	{CT0+0, CT1+0, CT2+13},	{CT0+0, CT1+0, CT2+12},	{CT0+0, CT1+0, CT2+11},	{CT0+0, CT1+0, CT2+10},	{CT0+0, CT1+0, CT2+9},	{CT0+0, CT1+0, CT2+8},	{CT0+0, CT1+0, CT2+7},	{CT0+0, CT1+0, CT2+6},	{CT0+0, CT1+0, CT2+5},	{CT0+0, CT1+0, CT2+4},	{CT0+0, CT1+0, CT2+3},	{CT0+0, CT1+0, CT2+2},	{CT0+0, CT1+0, CT2+1},	{CT0+0, CT1+0, CT2+1},	{CT0+0, CT1+0, CT2+0},
+			{CT0+0, CT1+0, CT2+0},	{CT0+1, CT1+0, CT2+0},	{CT0+2, CT1+0, CT2+0},	{CT0+3, CT1+0, CT2+0},	{CT0+4, CT1+0, CT2+0},	{CT0+5, CT1+0, CT2+0},	{CT0+6, CT1+0, CT2+0},	{CT0+7, CT1+0, CT2+0},	{CT0+8, CT1+0, CT2+0},	{CT0+9, CT1+0, CT2+0},	{CT0+10, CT1+0, CT2+0},	{CT0+11, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+14, CT1+0, CT2+0},	{CT0+13, CT1+0, CT2+0},	{CT0+12, CT1+0, CT2+1},	{CT0+11, CT1+0, CT2+2},	{CT0+10, CT1+0, CT2+3},	{CT0+9, CT1+0, CT2+4},	{CT0+8, CT1+0, CT2+5},	{CT0+7, CT1+0, CT2+6},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+10, CT1+0, CT2+12},	{CT0+10, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+10},	{CT0+13, CT1+0, CT2+8},	{CT0+13, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},
+			{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+7, CT1+0, CT2+7},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+9},	{CT0+12, CT1+0, CT2+10},	{CT0+12, CT1+0, CT2+10},	{CT0+10, CT1+0, CT2+12},	{CT0+10, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+9, CT1+0, CT2+12},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+10},	{CT0+13, CT1+0, CT2+8},	{CT0+13, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+8, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},	{CT0+13, CT1+0, CT2+13},
+
+			
+			
+		};
+	
+	// Aleatório3
+		public static void trafegoForcado(RodadaDeSimulacao rodada, Topologia to, No dados) {
+			
+				
+			if (dados == null) {
+				dados = new No();
+				dados.item = 0;
+				Debug.setMensagem("Agenda estabelecimento de LSP do slot:" + 0);
+				rodada.schedulep(3, 0, dados);
+
+			} else {
+
+				int slot = (int) dados.item;
+				/*if(slot%5==4)
+				{
+					try {
+						if(rodada.estatistica.devolucoes(300)>=5||rodada.estatistica.preempcoes(300)>=5)
+						{
+							Lsp LSPaux= new Lsp(); 
+							LSPaux.Carga=0; 
+							to.link[0].bamType = BAMType.PreemptionGBAM;
+							to.link[0].BCLTH= new double[]
+							{	000, //BC0 
+								000, //BC1
+								0  //BC2 Nunca mudar
+							};
+							LSPaux.CT=0; 
+				      		BAM.devolutionG(to.link[0],LSPaux);
+							
+							
+							to.link[0].BCHTL= new double[]
+							{	0, //BC0 Nunca mudar
+								000, //BC1
+								000 //BC2
+							};
+							
+							LSPaux.CT=2; 
+				      		BAM.preemptionG(to.link[0],LSPaux); 
+							
+						}
+						else if(rodada.estatistica.picoDeUtilizacaoDoEnlace(300,to.link[0])<=800)
+						{
+							to.link[0].bamType = BAMType.PreemptionGBAM;
+							to.link[0].BCLTH= new double[]
+							{	100, //BC0 
+								100, //BC1
+								0  //BC2 Nunca mudar
+							};
+							
+							to.link[0].BCHTL= new double[]
+							{	0, //BC0 Nunca mudar
+								100, //BC
+								100 //BC2
+							};
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (RrdException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				
+				
+				}
+				*/
+
+				for (int ct = 0; ct < ParametrosDSTE.MaxClassType; ct++) {
+
+					for (int i = 0; i < matriz[slot][ct]; i++) {
+						No dadosLSP = new No();
+
+						Lsp lsp = new Lsp(rodada);
+						lsp.CargaReduzida = 0;
+						lsp.src = 0; // id do router fonte
+						lsp.dest = 1; // id do router destino
+						lsp.CT = ct;
+						lsp.Carga = 10;//(int) GeradorDeNumerosAleatorios.uniform(5, 15);
+						lsp.tempoDeVida = 120;//(int) GeradorDeNumerosAleatorios.expntl(250);
+						dadosLSP.item = lsp;
+
+						Debug.setMensagem("Cria LSP " + ((Lsp) dadosLSP.item).ID + " - "
+								+ to.getRoteador(((Lsp) dadosLSP.item).src).getDescricao() + " -->"
+								+ to.getRoteador(((Lsp) dadosLSP.item).dest).getDescricao());
+						rodada.schedulep(1, GeradorDeNumerosAleatorios.uniform(0, 59), dadosLSP);
+
+					}
+				}
+				Debug.setMensagem("Agenda estabelecimento de LSP do slot:" + slot);
+				dados.item = slot + 1;
+				rodada.schedulep(3, 60, dados);
+
+			}
+
+		}
 	public static void trafegoDeterministico(RodadaDeSimulacao rodada, Topologia to, No dados) {
 
 		// Inicializar Tráfego
